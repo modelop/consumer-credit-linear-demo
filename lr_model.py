adbeec621cadc5f70c5a268bfaf997aad82901b3
Dave Trier <DaveTrier@ModelOpAdmins-MacBook-Pro.local>
2024-08-21 10:18:28 -0600

    modified shap output structure

--- lr_model.py
+++ lr_model.py
@@ -1,5 +1,6 @@
 # modelop.schema.0: input_schema.avsc
 # modelop.schema.1: output_schema.avsc
+import logging
 
 import pandas as pd
 import numpy as np
@@ -17,10 +18,10 @@ import shap
 import pickle
 
 
-#modelop.init
+# modelop.init
 def begin():
     global explainer, lr_model, threshold, features, rent_ratio, gamma_args, \
-            logit_int_rate_mean
+        logit_int_rate_mean
     model_artifacts = pickle.load(open("model_artifacts.pkl", "rb"))
     explainer = model_artifacts['explainer']
     lr_model = model_artifacts['lr_model']
@@ -31,16 +32,18 @@ def begin():
     logit_int_rate_mean = model_artifacts['logit_int_rate_mean']
     pass
 
-#modelop.score
+
+# modelop.score
 def action(datum):
     datum = pd.DataFrame(datum, index=[0])
     prep_datum = preprocess(datum)
     datum = pd.concat([datum, prep_datum], axis=1)
     datum['probability'] = prediction(datum)
     datum['prediction'] = datum.probability \
-                          .apply(lambda x: x > threshold).astype(int)
+        .apply(lambda x: x > threshold).astype(int)
     yield datum.loc[:, ['id', 'probability', 'prediction']] \
-            .to_dict(orient='records')[0]
+        .to_dict(orient='records')[0]
+
 
 def preprocess(data):
     prep_data = pd.DataFrame(index=data.index)
@@ -51,40 +54,43 @@ def preprocess(data):
     prep_data["rent_indicator"] = data.home_ownership.isin(['RENT']).astype(int)
     return prep_data
 
-def prediction(data):
-    return lr_model.predict_proba(data.loc[:, features])[:,1]
 
-#modelop.metrics
+def prediction(data):
+    return lr_model.predict_proba(data.loc[:, features])[:, 1]
+
+
+# modelop.metrics
 def metrics(data):
     metrics = {}
     prep_data = preprocess(data)
     data = pd.concat([data, prep_data], axis=1)
     data.loc[:, 'probabilities'] = prediction(data)
     data.loc[:, 'predictions'] = data.probabilities \
-                                     .apply(lambda x: threshold > x) \
-                                     .astype(int)
-    
-    if is_validated(data):
-       f1 = f1_score(data.loan_status, data.predictions)
-       cm = confusion_matrix(data.loan_status, data.predictions)
-       labels = ['Fully Paid', 'Charged Off']
-       cm = matrix_to_dicts(cm, labels)
-       fpr, tpr, thres = roc_curve(data.loan_status, data.probabilities)
-       auc_val = roc_auc_score(data.loan_status, data.probabilities)
-       rc = [{'fpr': x[0], 'tpr': x[1]} for x in list(zip(fpr, tpr))]
-       metrics['f1_score'] = f1
-       metrics['confusion_matrix'] = cm
-       metrics['auc'] = auc_val
-       metrics['ROC'] = rc
-       metrics['bias'] = get_bias_metrics(data)
+        .apply(lambda x: threshold > x) \
+        .astype(int)
 
-    #metrics['drift_metrics'] = get_drift_metrics(data)
+    # if is_validated(data):
+    #    f1 = f1_score(data.loan_status, data.predictions)
+    #    cm = confusion_matrix(data.loan_status, data.predictions)
+    #    labels = ['Fully Paid', 'Charged Off']
+    #    cm = matrix_to_dicts(cm, labels)
+    #    fpr, tpr, thres = roc_curve(data.loan_status, data.probabilities)
+    #    auc_val = roc_auc_score(data.loan_status, data.probabilities)
+    #    rc = [{'fpr': x[0], 'tpr': x[1]} for x in list(zip(fpr, tpr))]
+    #    metrics['f1_score'] = f1
+    #    metrics['confusion_matrix'] = cm
+    #    metrics['auc'] = auc_val
+    #    metrics['ROC'] = rc
+    #    metrics['bias'] = get_bias_metrics(data)
+
+    # metrics['drift_metrics'] = get_drift_metrics(data)
     metrics['shap'] = get_shap_values(data)
     yield metrics
 
 
 def is_validated(data):
-    return ('loan_status' in data.columns)
+    return 'loan_status' in data.columns
+
 
 def get_bias_metrics(data):
     bias = Bias()
@@ -93,7 +99,7 @@ def get_bias_metrics(data):
     new_columns = ['score', 'label_value', 'forty_plus_indicator']
     scored_data = data.loc[:, old_columns]
     renamer = dict(zip(scored_data.columns, new_columns))
-    scored_data = scored_data.rename(columns = renamer)
+    scored_data = scored_data.rename(columns=renamer)
 
     data_processed, _ = preprocess_input_df(scored_data)
     xtab, _ = group.get_crosstabs(data_processed)
@@ -111,38 +117,41 @@ def get_bias_metrics(data):
     abs_metrics = absolute_metrics_df.where(pd.notnull(absolute_metrics_df),
                                             None).to_dict(orient='records')
     disp_metrics = disparity_metrics_df.where(pd.notnull(disparity_metrics_df),
-                                            None).to_dict(orient='records')
-    return dict(absolute_metrics = abs_metrics,
-                disparity_metrics = disp_metrics)
+                                              None).to_dict(orient='records')
+    return dict(absolute_metrics=abs_metrics,
+                disparity_metrics=disp_metrics)
+
 
 def get_shap_values(data):
     shap_values = explainer.shap_values(data.loc[:, features])
     shap_values = np.mean(abs(shap_values), axis=0).tolist()
-    shap_values = dict(zip(features, shap_values))
+    shap_values = dict(zip(features, [shap_values)])
     sorted_shap_values = {k: v for k, v in sorted(shap_values.items(),
                                                   key=lambda x: x[1])}
+    logging.log("info")
     final_shap_values = {"title": "Shap Explainability", "x_axis_label": "Shap Values", "y_axis_label": "Features",
-                         "rotated": True, "data": sorted_shap_values}
+                         "rotated": True, "data": {sorted_shap_values}}
     return final_shap_values
+
 
 def get_drift_metrics(data):
     num_of_renters = data.rent_indicator.sum()
     size_of_sample = data.shape[0]
-    rent_feat_binom_pvalue = binom_test(x=num_of_renters, 
-                                        n=size_of_sample, 
+    rent_feat_binom_pvalue = binom_test(x=num_of_renters,
+                                        n=size_of_sample,
                                         p=rent_ratio)
 
     logit_int_rate_pvalue = ttest_1samp(a=data.logit_int_rate,
                                         popmean=logit_int_rate_mean)[1]
     pred_log_probs = lr_model.predict_log_proba(X=data.loc[:, features])[:, 1]
-    neg_log_probs = -1*pred_log_probs
-    output_logprob_pvalue = kstest(neg_log_probs, 
-                                   'gamma', 
+    neg_log_probs = -1 * pred_log_probs
+    output_logprob_pvalue = kstest(neg_log_probs,
+                                   'gamma',
                                    args=gamma_args)[1]
     drift_metrics = dict(renters_binom_pvalue=rent_feat_binom_pvalue,
                          output_logprob_pvalue=output_logprob_pvalue,
-                        logit_int_rate_ttest_pvalue=logit_int_rate_pvalue)
-                
+                         logit_int_rate_ttest_pvalue=logit_int_rate_pvalue)
+
     return drift_metrics
 
 
@@ -151,6 +160,3 @@ def matrix_to_dicts(matrix, labels):
     for idx, label in enumerate(labels):
         cm.append(dict(zip(labels, matrix[idx, :].tolist())))
     return cm
-
-
-
